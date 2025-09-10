6e127b4c97c1be5aad127e704a2cd2586aa3b635
LmarDark <lucasmatheusalero@gmail.com>
2025-09-10 13:44:07 -0400

    feat: added MX, CNAME, NS, TXT and AAAA types

--- app/Http/Controllers/Api/DnsLookupController.php
+++ app/Http/Controllers/Api/DnsLookupController.php
@@ -9,10 +9,54 @@ class DnsLookupController extends Controller
 {
     public function lookup_DNS_A(DnsLookupRequest $request)
     {
-        $domain     = $request->input('domain');
-        # $dns_server = $request->input('dnsserver') ?? '8.8.8.8';
+        $domain = $request->input('domain');
 
         $result = dns_get_record($domain, DNS_A);
+
+        return empty($result) ? response()->json(['message' => 'No results']) : response()->json($result);
+    }
+
+    public function lookup_DNS_MX(DnsLookupRequest $request)
+    {
+        $domain = $request->input('domain');
+
+        $result = dns_get_record($domain, DNS_MX);
+
+        return empty($result) ? response()->json(['message' => 'No results']) : response()->json($result);
+    }
+
+    public function lookup_DNS_CNAME(DnsLookupRequest $request)
+    {
+        $domain = $request->input('domain');
+
+        $result = dns_get_record($domain, DNS_CNAME);
+
+        return empty($result) ? response()->json(['message' => 'No results']) : response()->json($result);
+    }
+
+    public function lookup_DNS_NS(DnsLookupRequest $request)
+    {
+        $domain = $request->input('domain');
+
+        $result = dns_get_record($domain, DNS_NS);
+        
+        return empty($result) ? response()->json(['message' => 'No results']) : response()->json($result);
+    }
+
+    public function lookup_DNS_TXT(DnsLookupRequest $request)
+    {
+        $domain = $request->input('domain');
+
+        $result = dns_get_record($domain, DNS_TXT);
+
+        return empty($result) ? response()->json(['message' => 'No results']) : response()->json($result);
+    }
+
+    public function lookup_DNS_AAAA(DnsLookupRequest $request)
+    {
+        $domain = $request->input('domain');
+
+        $result = dns_get_record($domain, DNS_AAAA);
 
         return empty($result) ? response()->json(['message' => 'No results']) : response()->json($result);
     }
