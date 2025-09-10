8ef9fd62a6f6a083395c044842cc1f4184712347
LmarDark <lucasmatheusalero@gmail.com>
2025-09-10 13:41:52 -0400

    refactor: prefix added

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,11 @@
 use App\Http\Controllers\Api\DnsLookupController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/lookup', [DnsLookupController::class, 'lookup_DNS_A']);
+Route::prefix('/dns/lookup/')->controller(DnsLookupController::class)->group(function () {
+    Route::post('/a', 'lookup_DNS_A');
+    Route::post('/mx', 'lookup_DNS_MX');
+    Route::post('/cname', 'lookup_DNS_CNAME');
+    Route::post('/ns', 'lookup_DNS_NS');
+    Route::post('/txt', 'lookup_DNS_TXT');
+    Route::post('/aaaa', 'lookup_DNS_AAAA');
+});
