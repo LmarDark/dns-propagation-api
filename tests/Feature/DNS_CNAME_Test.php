9b2ead04cff82bb139d5971a689189b3bd89f629
LmarDark <lucasmatheusalero@gmail.com>
2025-09-11 09:53:08 -0400

    refactor: tests refactored

--- tests/Feature/DNS_CNAME_Test.php
+++ tests/Feature/DNS_CNAME_Test.php
@@ -6,15 +6,3 @@ test('the aplication returns 200 response', function () {
     $response->assertStatus(200)
              ->assertJsonStructure([['host', 'type', 'target']]);
 });
-
-test('the aplication returns 302 response', function () {
-    $response = $this->get('/api/dns/lookup/cname?domain=');
-
-    $response->assertStatus(302);
-});
-
-test('the aplication returns 500 response', function () {
-    $response = $this->get('/api/dns/lookup/cname?domain=*');
-
-    $response->assertStatus(500);
-});
